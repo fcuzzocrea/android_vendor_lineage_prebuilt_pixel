7a7fde99f2169b346b32d7a03f61f03a2878ef46
Francescodario Cuzzocrea <bosconovic@gmail.com>
2024-05-20 23:35:28 +0200

    pixel: import prebuilts from aospa
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

---
+++ packages.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2023 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_PACKAGES += \
+    DeviceIntelligenceNetworkPrebuilt \
+    DevicePersonalizationPrebuiltPixel2020 \
+    TurboAdapter \
+    com.google.android.apps.dialer.call_recording_audio.features
